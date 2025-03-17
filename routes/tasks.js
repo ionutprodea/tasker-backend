034f4beebafc77f7ca801876c61fb6b9b14bdbc8
ionutprodea <147700111+ionutprodea@users.noreply.github.com>
2025-03-17 20:50:01 +0200

    Added task limit per user

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,6 +8,8 @@ const auth = require("../middleware/auth");
 router.post("/",auth, async(req, res) => {
     const {error} = validateTask(req.body);
     if(error) return res.status(400).send(error.details[0].message);
+    const tasks = await Task.find({userId: req.user._id});
+    if(tasks.length > 49) return res.status(403).send("Task limit reached. Contact admin for more info.");
     let task = await Task.findOne( {task: req.body.task, date: req.body.date});
     if(task) return res.status(400).send('The task already exists for the selected date');
     task = new Task({task: req.body.task, importance: req.body.importance, date: req.body.date, details: req.body.details, userId: req.user._id});
