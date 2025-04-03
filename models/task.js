1019d6f5c03d6fab28d794ea93f9c1f7d2be6d69
ionutprodea <147700111+ionutprodea@users.noreply.github.com>
2025-04-03 20:33:31 +0300

    Updated taskSchema

--- models/task.js
+++ models/task.js
@@ -41,6 +41,11 @@ const taskSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true 
+    },
+
+    checked: {
+        type: Boolean,
+        default: false
     }
 });
 
