225e4e76ba108a70ad08cb1fb1bf23777c0bd92f
ionutprodea <147700111+ionutprodea@users.noreply.github.com>
2025-04-28 13:22:07 +0300

    Added env privatekey, render test

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ function auth(req, res, next) {
     if (!token) return res.status(401).send('Access denied. No token provided.')
     
     try {
-        const decoded = jwt.verify(token, "iYGa2wylF2X7ohVUeI37Hm26JKjtHa2n");
+        const decoded = jwt.verify(token, privateKey);
         req.user = decoded;
         next();
     }
--- models/user.js
+++ models/user.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema({
 })
 
 userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({_id: this._id}, "iYGa2wylF2X7ohVUeI37Hm26JKjtHa2n");
+    const token = jwt.sign({_id: this._id}, privateKey);
     return token;
 }
 
